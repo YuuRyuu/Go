fc56b5fad0db8dee8e5f0deef615e4838a8d865b
Miguel Salinas <miguel.salinase10@gmail.com>
2024-05-21 06:39:19 +0900

    add command-line argument example

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 // This is how you import packages.
 import (
 	"fmt"
+	"os"
 
 	"rsc.io/quote/v4"
 )
@@ -10,4 +11,12 @@ import (
 // This is the main function
 func main() {
 	fmt.Println(quote.Go())
+
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	fmt.Println(s)
 }
