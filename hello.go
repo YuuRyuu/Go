d011cf0e3f9005987bd672b727db28a1472755ed
Miguel Salinas <miguel.salinase10@gmail.com>
2024-05-21 02:59:12 +0900

    add external package and go.sum file

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+// This is how you import packages.
+import (
+	"fmt"
 
+	"rsc.io/quote/v4"
+)
+
+// This is the main function
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
 }
